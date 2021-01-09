f9f1fc00ec37f2afed1d26484b50fdab077a8f0a
glhillman <glhillman@gmail.com>
2021-01-09 11:04:27 -0700

    Initial commit

---
+++ Day02/Box.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02
+{
+    public class Box
+    {
+        public Box(int length, int width, int height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+
+            _sortedDimensions = new int[] { Length, Width, Height };
+
+            // sort the values
+            Array.Sort(_sortedDimensions);
+
+        }
+
+        public int AreaPlusSlack()
+        {
+            return Area() + Slack();
+        }
+
+        public int Area()
+        {
+            return (2 * Length * Width) + (2 * Width * Height) + (2 * Height * Length);
+        }
+
+        public int Slack()
+        {
+            return _sortedDimensions[0] * _sortedDimensions[1];
+        }
+
+        public int TotalRibbon()
+        {
+            return Ribbon() + Bow();
+        }
+        
+        public int Ribbon()
+        {
+            return 2 * _sortedDimensions[0] + 2 * _sortedDimensions[1];
+        }
+
+        public int Bow()
+        {
+            return Length * Width * Height;
+        }
+
+        public int Length { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        private int[] _sortedDimensions;
+    }
+}
---
+++ Day02/Day02.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02
+{
+    public class Day02
+    {
+        private List<Box> _boxes;
+
+        public Day02()
+        {
+            _boxes = new List<Box>();
+            LoadData();
+        }
+
+        public void Part1AndPart2()
+        {
+            int paper = 0;
+            int ribbon = 0;
+
+            foreach (Box box in _boxes)
+            {
+                paper += box.AreaPlusSlack();
+                ribbon += box.TotalRibbon();
+            }
+
+            Console.WriteLine("Part1: {0}", paper);
+            Console.WriteLine("Part1: {0}", ribbon);
+        }
+
+        private void LoadData()
+        {
+            string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\input.txt";
+
+            if (File.Exists(inputFile))
+            {
+                string line;
+                StreamReader file = new StreamReader(inputFile);
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] sides = line.Split('x');
+                    _boxes.Add(new Box(int.Parse(sides[0]), int.Parse(sides[1]), int.Parse(sides[2])));
+                }
+
+                file.Close();
+            }
+        }
+
+    }
+}
