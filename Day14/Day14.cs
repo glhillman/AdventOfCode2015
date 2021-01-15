b8379944a08dafe866e01d8638b70d910a8f231a
glhillman <glhillman@gmail.com>
2021-01-15 11:48:17 -0700

    Initial commit

---
+++ Day14/Day14.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14
+{
+    public class Day14
+    {
+        List<Deer> _deer = new List<Deer>();
+        public Day14()
+        {
+            LoadData();
+        }
+
+        public void Part1()
+        {
+            int maxDistance = int.MinValue;
+
+            foreach (Deer deer in _deer)
+            {
+                maxDistance = Math.Max(deer.RunFor(2503), maxDistance);
+            }
+
+            Console.WriteLine("Part1: {0}", maxDistance);
+        }
+
+        public void Part2()
+        {
+            for (int i = 0; i < _deer.Count; i++)
+            {
+                _deer[i].Reset();
+            }
+
+            for (int i = 0; i < 2503; i++)
+            {
+                int leadDist = int.MinValue;
+                for (int j = 0; j < _deer.Count; j++)
+                {
+                    int dist = _deer[j].Step();
+                    if (dist > leadDist)
+                    {
+                        leadDist = dist;
+                    }
+                }
+                foreach (Deer deer in _deer)
+                {
+                    if (deer.Distance == leadDist)
+                    {
+                        deer.AddPoint();
+                    }
+                }
+            }
+
+            int maxPoints = _deer.Max(d => d.Points);
+
+            Console.WriteLine("Part2: {0}", maxPoints);
+        }
+
+        private void LoadData()
+        {
+            string inputFile = AppDomain.CurrentDomain.BaseDirectory + @"..\..\input.txt";
+
+            if (File.Exists(inputFile))
+            {
+                string line;
+                StreamReader file = new StreamReader(inputFile);
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] tokens = line.Split(' ');
+                    _deer.Add(new Deer(tokens[0], int.Parse(tokens[3]), int.Parse(tokens[6]), int.Parse(tokens[13])));
+                }
+
+                file.Close();
+            }
+        }
+
+    }
+}
---
+++ Day14/Deer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14
+{
+    public class Deer
+    {
+        public Deer(string name, int speed, int runTime, int restTime)
+        {
+            Name = name;
+            Speed = speed;
+            RunTime = runTime;
+            RestTime = restTime;
+        }
+
+        public void Reset()
+        {
+            Distance = 0;
+            RunLeft = RunTime;
+            RestLeft = 0;
+            Points = 0;
+        }
+
+        public int RunFor(int seconds)
+        {
+            
+            int second = 0;
+
+            Reset();
+
+            while (second < seconds)
+            {
+                Step();
+                second++;
+            }
+
+            return Distance;
+        }
+
+        public int Step()
+        {
+            if (RunLeft > 0)
+            {
+                Distance += Speed;
+                if (--RunLeft == 0)
+                {
+                    RestLeft = RestTime;
+                }
+            }
+            else if (RestLeft > 0)
+            {
+                if (--RestLeft == 0)
+                {
+                    RunLeft = RunTime;
+                }
+            }
+
+            return Distance;
+        }
+
+        public void AddPoint()
+        {
+            Points++;
+        }
+
+        public string Name { get; private set; }
+        private int Speed { get; set; }
+        private int RunTime { get;  set; }
+        private int RestTime { get; set; }
+        private int RunLeft { get; set; }
+        private int RestLeft { get; set; }
+        public int Distance { get; set; }
+        public int Points { get; set; }
+    }
+}
