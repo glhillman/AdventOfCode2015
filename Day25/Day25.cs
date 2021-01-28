a82cdd90125fd01d3bdfa48ae9ae74f4e69cb477
glhillman <glhillman@gmail.com>
2021-01-28 08:36:09 -0700

    Initial commit

---
+++ Day25/Day25.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day25
+{
+    class Day25
+    {
+        int targetRow = 2978;
+        int targetCol = 3083;
+
+        public Day25()
+        {
+        }
+
+        public void Part1()
+        {
+            long value = 20151125;
+            int anchorRow = 2;
+            int col = 1;
+            int row = anchorRow;
+            while (true)
+            {
+                value = (value * 252533) % 33554393;
+                if (row == targetRow && col == targetCol)
+                {
+                    break;
+                }
+                if (row > 1)
+                {
+                    col++;
+                    row--;
+                }
+                else
+                { 
+                    row = ++anchorRow;
+                    col = 1;
+                }
+            }
+
+            Console.WriteLine("Part1: {0}", value);
+        }
+    }
+}
---
+++ Day25/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Day25 day25 = new Day25();
+
+            day25.Part1();
+
+            Console.Write("Press Enter to continue...");
+            Console.ReadLine();
+
+        }
+    }
+}
