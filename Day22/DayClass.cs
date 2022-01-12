e5d81f01d01f57706d5eff0d314d210efa00204c
glhillman <glhillman@gmail.com>
2022-01-12 09:05:53 -0700

    Day22 minor cleanup

--- Day22/DayClass.cs
+++ Day22/DayClass.cs
@@ -15,23 +15,13 @@ namespace Day22
         public int _personHitCountInit = 50;
         public int _personManaInit = 500;
 
-        public int _bossHitCount;
-        public int _bossDamage;
-        public int _personHitCount;
-        public int _personMana;
-        public int _personArmor;
-
-        public int _totalMana;
-
-        public int _shieldTimer = 0;
-        public int _poisonTimer = 0;
-        public int _rechargeTimer = 0;
-
         public const int _missileID = 0;
         public const int _drainID = 1;
         public const int _shieldID = 2;
         public const int _poisonID = 3;
         public const int _rechargeID = 4;
+
+        int _totalMana;
 
         public int[] _spellCosts = { 53, 73, 113, 173, 229 };
         public int[] _spellTimers = { 0, 0, 0, 0, 0 };
@@ -59,7 +49,7 @@ namespace Day22
         public void Part2()
         {
             int minMana = int.MaxValue;
-            for (int i = 0; i < 15000; i++)
+            for (int i = 0; i < 25000; i++)
             {
                 if (PlayGame(true) == true)
                 {
@@ -74,26 +64,23 @@ namespace Day22
             Console.WriteLine("Part2: {0}", minMana);
         }
 
-        public void InitState()
+         public bool? PlayGame(bool hard) // returns true if person wins, false if boss wins
         {
-            _bossHitCount = _bossHitCountInit;
-            _bossDamage = _bossDamageInit;
-            _personHitCount = _personHitCountInit;
-            _personMana = _personManaInit;
+            bool? personWon = null;
+            bool personTurn = true;
+            int bossHitCount = _bossHitCountInit;
+            int bossDamage = _bossDamageInit;
+            int personHitCount = _personHitCountInit;
+            int personMana = _personManaInit;
+            int personArmor = 0;
+
             _totalMana = 0;
             for (int i = 0; i < 5; i++)
             {
                 _spellTimers[i] = 0;
             }
-        }
 
-        public bool? PlayGame(bool hard) // returns true if person wins, false if boss wins
-        {
-            bool? personWon = null;
-            bool personTurn = true;
-            InitState();
-
-            while (personWon == null && _bossHitCount > 0 && _personHitCount > 0)
+            while (personWon == null && bossHitCount > 0 && personHitCount > 0)
             {
                 // first apply any spells on timers
                 if (_spellTimers[_shieldID] > 0)
@@ -102,102 +89,98 @@ namespace Day22
                 }
                 if (_spellTimers[_shieldID] == 0)
                 {
-                    _personArmor = 0;
+                    personArmor = 0;
                 }
                 if (_spellTimers[_poisonID] > 0)
                 {
-                    _bossHitCount -= 3;
+                    bossHitCount -= 3;
                     _spellTimers[_poisonID]--;
                 }
                 if (_spellTimers[_rechargeID] > 0)
                 {
-                    _personMana += 101;
+                    personMana += 101;
                     _spellTimers[_rechargeID]--;
                 }
-                if (_bossHitCount <= 0)
+                if (bossHitCount <= 0)
                 {
                     personWon = true;
                     break;
                 }
 
-                if (personWon == null)
+                // Now toggle between Person and Boss turns
+                // Person is more complex - create a spell & cast it
+                if (personTurn)
                 {
-                    // Now toggle between Person and Boss turns
-                    // Person is more complex - create a spell & cast it
-                    if (personTurn)
+                    if (hard)
                     {
-                        if (hard)
+                        if (--personHitCount <= 0)
                         {
-                            if (--_personHitCount <= 0)
-                            {
-                                personWon = false;
-                                break;
-                            }
-                        }
-
-                        int? spellIndex = GetRandomSpell();
-                        if (spellIndex.HasValue)
-                        {
-                            switch (spellIndex) // will return 0..4 for a spell that we can afford & doesn't have an active timer
-                            {
-                                case _missileID:
-                                    _bossHitCount -= 4;
-                                    break;
-                                case _drainID:
-                                    _personHitCount += 2;
-                                    _bossHitCount -= 2;
-                                    break;
-                                case _shieldID:
-                                    _spellTimers[_shieldID] = 6;
-                                    _personArmor = 7;
-                                    break;
-                                case _poisonID:
-                                    _spellTimers[_poisonID] = 6;
-                                    break;
-                                case _rechargeID:
-                                    _spellTimers[_rechargeID] = 5;
-                                    break;
-                                default:
-                                    break;
-                            }
-                            _personMana -= _spellCosts[spellIndex.Value];
-                            _totalMana += _spellCosts[spellIndex.Value];
-                            if (_bossHitCount <= 0)
-                            {
-                                personWon = true;
-                            }
-                        }
-                        else
-                        {
+                            personWon = false;
                             break;
+                        }
+                    }
+
+                    int? spellIndex = GetRandomSpell(personMana);
+                    if (spellIndex.HasValue)
+                    {
+                        switch (spellIndex) // will return 0..4 for a spell that we can afford & doesn't have an active timer
+                        {
+                            case _missileID:
+                                bossHitCount -= 4;
+                                break;
+                            case _drainID:
+                                personHitCount += 2;
+                                bossHitCount -= 2;
+                                break;
+                            case _shieldID:
+                                _spellTimers[_shieldID] = 6;
+                                personArmor = 7;
+                                break;
+                            case _poisonID:
+                                _spellTimers[_poisonID] = 6;
+                                break;
+                            case _rechargeID:
+                                _spellTimers[_rechargeID] = 5;
+                                break;
+                            default:
+                                break;
+                        }
+                        personMana -= _spellCosts[spellIndex.Value];
+                        _totalMana += _spellCosts[spellIndex.Value];
+                        if (bossHitCount <= 0)
+                        {
+                            personWon = true;
                         }
                     }
                     else
                     {
-                        _personHitCount -= Math.Max(_bossDamage - _personArmor, 1);
-                        if (_personHitCount <= 0)
-                        {
-                            personWon = false;
-                        }
+                        break;
                     }
-                    personTurn = !personTurn;
                 }
-
+                else
+                {
+                    personHitCount -= Math.Max(bossDamage - personArmor, 1);
+                    if (personHitCount <= 0)
+                    {
+                        personWon = false;
+                    }
+                }
+                personTurn = !personTurn;
             }
 
             return personWon;
         }
 
-        public int? GetRandomSpell()
+        public int? GetRandomSpell(int personMana)
         {
             int? spellIndex = null;
 
-            if (_personMana >= _spellCosts[0])
+            if (personMana >= _spellCosts[0])
             {
                 do
                 {
                     spellIndex = _rand.Next(5);
-                } while (_spellCosts[spellIndex.Value] > _personMana || _spellTimers[spellIndex.Value] > 0);
+                } while (_spellCosts[spellIndex.Value] > personMana || _spellTimers[spellIndex.Value] > 0);
             }
 
             return spellIndex;
